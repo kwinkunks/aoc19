6459d63073b139ac53010ebe740e7cc385569e5d
kwinkunks <matt@agilegeoscience.com>
2019-12-01 14:23:38 +0000

    Switched to integer division

--- day1.py
+++ day1.py
@@ -16,6 +16,8 @@ def get_data() -> list:
 def part1(data: list) -> int:
     """Part 1. Floor... we can use int().
 
+    Update: Didn't occur to me earlier to use integer division, which I prefer.
+
     Tests
     >>> part1([12])
     2
@@ -26,13 +28,13 @@ def part1(data: list) -> int:
     >>> part1([100756])
     33583
     """
-    return sum(int(x/3) - 2 for x in data)
+    return sum(x // 3 - 2 for x in data)
 
 
 def sum_fuel(fuel: int) -> int:
     """Recursively sum the fuel.
     """
-    new = int(fuel/3) - 2
+    new = fuel // 3 - 2
     if new < 1:
         return 0
     else:
@@ -50,7 +52,7 @@ def part2(data: list) -> int:
     >>> part2([100756])
     50346
     """
-    results = [int(x/3) - 2 for x in data]
+    results = [x // 3 - 2 for x in data]
     extras = [sum_fuel(r) for r in results]
     return sum(extras) + sum(results)
 
